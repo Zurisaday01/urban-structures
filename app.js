d5f346c1744d1807cf33583220bda3370c4af319
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2023-10-26 14:51:47 -0600

    advance

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav__links");
 const links = document.querySelectorAll(".nav__links li");
@@ -14,4 +15,23 @@ hamburger.addEventListener('click', ()=>{
 
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loaderOverlay = document.querySelector('.loader-overlay');
+
+    window.addEventListener('beforeunload', function () {
+        // You may choose to show the loader when the user tries to leave the page
+        loaderOverlay.style.display = 'block';
+    });
+
+    window.addEventListener('unload', function () {
+        // This will hide the loader when the user is leaving the page
+        loaderOverlay.style.display = 'none';
+    });
+
+    setTimeout(function () {
+        // This will hide the loader after a delay
+        loaderOverlay.style.display = 'none';
+    }, 1800); 
 });
